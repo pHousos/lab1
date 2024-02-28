c9a80113833fc77e159e03fc1c4a88e5810e21ed
pHousos <up1083869@ac.upatras.gr>
2024-02-28 15:54:07 +0200

    cookies name and duration

--- lab1.py
+++ lab1.py
@@ -1,21 +1,26 @@
 import requests
+from datetime import datetime
 
-url = input("Enter the URL: ")  # ζήτησε από τον χρήστη να εισάγει το URL
+url = input("Enter the URL: ")
 
 try:
     with requests.get(url) as response:
-        response.raise_for_status()  # Ελέγχει αν υπάρχει σφάλμα κατά την αίτηση
+        response.raise_for_status()
         
-        # Εκτύπωση του λογισμικού του εξυπηρετητή
         server_software = response.headers.get('Server')
         if server_software:
             print(f"The server is powered by: {server_software}")
         else:
             print("Server software information not available.")
         
-        # Έλεγχος για τη χρήση cookies
         if 'Set-Cookie' in response.headers:
             print("The page uses cookies.")
+            
+            print("\nCookies:")
+            for cookie in response.cookies:
+                print(f"Name: {cookie.name}")
+                expiration_date = datetime.fromtimestamp(cookie.expires)
+                print(f"Expires: {expiration_date}")
         else:
             print("The page does not use cookies.")
 except requests.RequestException as e:
